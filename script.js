4c9b6a75e87259d1957cbcfc1b1afaf5c90ed295
Emil <emin-mamedov-99@inbox.ru>
2024-07-25 11:49:47 +0400

    lasT

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+
+menuBtn.addEventListener('click',function(){
+    menu.classList.add('active');
+});
+
+
+
+
